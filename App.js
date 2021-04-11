b4290ce1d5b7cb059da3d73225c5a4fde6893ed9
nicolas.stophe@gmail.com <nicolas.stophe@live.com>
2021-04-11 20:49:41 +0200

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './components/Timer/reducers'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Timer from './components/Timer'
+
+const store = createStore(reducer)
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Timer />
+      </Provider>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Timer/index.js
@@ -0,0 +1,111 @@
+import React from 'react'
+import { StyleSheet, Text, View, StatusBar, Alert } from 'react-native'
+
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { actionCreators as actions } from './actions'
+
+import Button from '../Button'
+
+function formatTime(time) {
+    let minutes = Math.floor(time / 60)
+    time -= minutes * 60
+    let seconds = parseInt(time % 60, 10)
+    return `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
+}
+
+class Timer extends React.Component {
+
+    componentWillReceiveProps(nextProps) {
+        const currentProps = this.props
+        if (!currentProps.isPlaying && nextProps.isPlaying) {
+            // start the interval
+            const timerInterval = setInterval(() => {
+                currentProps.addSecond()
+            }, 1000)
+            this.setState({ timerInterval })
+        } else if (currentProps.isPlaying && !nextProps.isPlaying) {
+            // stop the interval
+            clearInterval(this.state.timerInterval)
+        }
+    }
+    render() {
+        const {
+            isPlaying,
+            elapsedTime,
+            timerDuration,
+            startTimer,
+            restartTimer
+        } = this.props
+
+        return (
+            <View style={styles.container}>
+                <StatusBar barStyle={'light-content'} />
+                <View style={styles.upper}>
+                    <Text style={styles.time}>
+                        {formatTime(timerDuration - elapsedTime)}
+                    </Text>
+                </View>
+                <View style={styles.lower}>
+                    {!isPlaying && (
+                        <Button
+                            iconName='play-circle'
+                            onPress={startTimer}
+                        />
+                    )}
+                    {isPlaying && (
+                        <Button
+                            iconName="stop-circle"
+                            onPress={restartTimer}
+                        />
+                    )}
+
+                </View>
+            </View>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    const { isPlaying, elapsedTime, timerDuration } = state
+    return {
+        isPlaying,
+        elapsedTime,
+        timerDuration
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        startTimer: bindActionCreators(actions.startTimer, dispatch),
+        restartTimer: bindActionCreators(actions.restartTimer, dispatch),
+        addSecond: bindActionCreators(actions.addSecond, dispatch)
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000000',
+    },
+    upper: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    lower: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    time: {
+        color: '#ffffff',
+        fontSize: 120,
+        fontWeight: '100'
+    }
+})
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Timer)
